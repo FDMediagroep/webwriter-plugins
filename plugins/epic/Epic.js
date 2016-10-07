182fb100c5e24c4fe1181b7d26f2e7474ada1047
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-10-07 13:30:34 +0200

    WBWRI-530 Added Epic plugin

--- plugins/epic/Epic.js
+++ plugins/epic/Epic.js
@@ -3,25 +3,18 @@
 function Epic() {
 }
 
-/**
- * Stockticker plugin schema definition
- *
- * @type {{name: string, vendor: string, node: *, converter: *, component: *, tool: *[], command: *[]}}
- */
-
 Epic.prototype.schema = {
 
-    name: 'epic',
-    vendor: 'nl.fdmg',
+  name: 'epic',
+  vendor: 'nl.fdmg',
 
-    uicomponents: {
-        sidebar: {
-            main: [
-                require('./EpicMainComponent')
-            ]
-        }
+  uicomponents: {
+    sidebar: {
+      main: [
+        require('./EpicMainComponent')
+      ]
     }
-
+  }
 };
 
 module.exports = Epic;
--- plugins/epic/EpicMainComponent.js
+++ plugins/epic/EpicMainComponent.js
@@ -2,49 +2,130 @@
 
 var Component = require('substance/ui/Component');
 var $$ = Component.$$;
-var EpicSearchComponent = require('./EpicSearchComponent');
-var EpicListComponent = require('./EpicListComponent');
+var jQuery = require('substance/util/jquery');
+var Icon = require('substance/ui/FontAwesomeIcon');
+var SearchComponent = require('vendor/nl.fdmg/universalsearch/UniversalSearchComponent');
+var genUUID = require('writer/utils/IdGenerator');
 
-// var __epic_state;
-
-function EpicMainComponent() { EpicMainComponent.super.apply(this, arguments); }
+function EpicMainComponent() {
+  EpicMainComponent.super.apply(this, arguments);
+  this.name = 'epic';
+}
 
 EpicMainComponent.Prototype = function() {
 
-    this.getInitialState = function () {
-        return {
-            existingEpics: this.context.api.getAuthors()
-        };
+  this.getInitialState = function() {
+    return {
+      existingEpic: this.getExistingEpic()
     };
+  };
 
-    this.render = function () {
-        var endpoint = this.context.api.getConfigValue('epic', 'endpoint');
+  this.render = function() {
 
-        var el = $$('div').ref('epicContainer').addClass('epic').append($$('h2').append('Epic'));
+    var existingEpic = this.state.existingEpic;
+    var epicEl = $$('span');
 
-        // Have to fix this
+    if (existingEpic) {
+      epicEl = $$('div')
+        .append(
+          $$('div')
+            .addClass('metadata__container')
+            .append(
+              $$('span')
+              .addClass('epic__name notClickable meta')
+              .append(existingEpic.name)
+            ),
+          $$('div')
+            .addClass('button__container')
+            .append(
+              $$('button')
+              .addClass('author__button--delete')
+              .append($$(Icon, { icon: 'fa-times' }))
+              .on('click', function() { this.removeEpic(); }.bind(this))
+            )
+        );
+    }
 
-        var searchComponent = $$(EpicSearchComponent, {
-            existingItems: this.state.existingEpics,
-            onSelect: this.addAuthor.bind(this),
-            onCreate: this.createAuthor.bind(this),
-            searchUrl: endpoint,
-            createdAllowed: false,
-            placeholderText: "Zoek epic"
+    return $$('div')
+      .ref('epicContainer')
+      .addClass('epic')
+      .append(
+        $$('h2').append(this.context.i18n.t('Epic')),
+        epicEl,
+        $$(SearchComponent, {
+          existingItems: existingEpic ? [existingEpic] : [],
+          doSearch: this.searchEpics.bind(this),
+          onSelect: this.setEpic.bind(this),
+          createAllowed: false,
+          placeholderText: "Set Epic"
+        }).ref('epicSearchComponent'),
+        $$('hr')
+      );
+  };
 
-        }).ref('epicSearchComponent');
+  this.reloadEpic = function() {
+    this.extendState({
+      existingEpic: this.getExistingEpic()
+    });
+  };
 
-        // var existingEpicsList = $$(EpicListComponent, {
-        //     existingEpics: this.state.existingEpics,
-        //     removeEpic: this.removeAuthor.bind(this)
-        // }).ref('existingAuthorList');
+  this.getExistingEpic = function() {
+    var links = this.context.api.getLinkByType(this.name, 'fdmg/epic');
+    var epic = links.pop();
 
-        el.append(searchComponent);
+    if (!!epic) {
+      return {
+        id: epic['@id'],
+        name: epic['@name']
+      };
+    }
 
-        return el;
+    return null;
+  };
 
-    };
+  this.searchEpics = function(q, cb) {
+    var endpoint = this.context.api.getConfigValue(this.name, 'endpoint');
 
+    this.extendState({ isSearching: true });
+
+    jQuery.ajax(endpoint + q, { data: { dataType: 'json' } })
+      .done(function(items) {
+
+        var epics = items.slice(0, 100).map(function(epic) {
+          return {
+            name: epic.title,
+            id: epic.id
+          }
+        });
+
+        cb(null, epics);
+      }.bind(this))
+      .fail(function(data, status, err) { console.error(err); cb(err, null); })
+      .always(function() { this.extendState({ isSearching: false }); }.bind(this));
+  };
+
+  this.setEpic = function(epic) {
+    this.removeEpic();
+
+    this.context.api.addLink(this.name, {
+      '@rel': this.name,
+      '@type': 'fdmg/epic',
+      '@id': epic.id,
+      '@name': epic.name,
+      '@uuid': genUUID()
+    });
+
+    this.reloadEpic();
+  };
+
+  this.removeEpic = function(epic) {
+    this.context.api.getLinkByType(this.name, 'fdmg/epic')
+      .forEach(function(epic) {
+        this.context.api.removeLinkByUUIDAndRel(this.name, epic['@uuid'], epic['@rel']);
+      }.bind(this));
+
+    this.reloadEpic();
+  };
 }
 
 Component.extend(EpicMainComponent);
