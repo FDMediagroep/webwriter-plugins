db2c3f427862222d99a9b3e5907ed3a21cc0acf3
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2016-10-11 17:25:58 +0200

    WBWRI-536 Made section list static

--- plugins/section/SectionComponent.js
+++ plugins/section/SectionComponent.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var SuperComponent = require('vendor/nl.fdmg/linkselector/LinkSelectorComponent');
+var jQuery = require('substance/util/jquery');
 
 var __section_state;
 
@@ -20,17 +21,24 @@ SectionComponent.Prototype = function() {
             'forcelistmode?': true
         });
 
-        var endpoint = this.context.api.getConfigValue('section', 'endpoint');
+        var sectionList = this.context.api.getConfigValue('section', 'sectionlist');
 
-        this.loadList(endpoint, function(items) {
-            items = this
-                .sortByAplhabet(items, 'title')
+        this.loadList(sectionList, function(items) {
+            items = sectionList
                 .map(function(item) { return { 'id': item.name, 'label': item.title } });
 
             items.unshift(this.getState().emptyitem);
             
             return items;
         }.bind(this));
+    }
+
+    this.loadList = function(sectionList, mapper) {
+        console.info('Loading items from "' + sectionList + '"');
+        console.log(sectionList, "sectionlist");
+        var sectionList = this.setItems(mapper(sectionList));
+        
+        return sectionList;
     }
 
     // Override this so that we can get the correct label
@@ -42,12 +50,24 @@ SectionComponent.Prototype = function() {
             console.log('no initial ' + this.getState().pluginname);
         } else {
             var id = links.shift()[prop];
+            console.log(id);
             var label = this.getItemLabelById(id);
 
+            // console.log(id, 'id', label, 'label');
             console.log('initial ' + this.getState().pluginname + ' is ' + label);
 
             return { 'id': id, 'label': label };
         }
+    }
+
+    this.getItemLabelById = function(id) {
+        console.log(id, "id @ getItemLabelById");
+        console.log(this.getState().items, "getstate items");
+
+        var item = this.getState().items
+            .filter(function(item) { return item.label === id; })
+            .pop();
+        return item ? item.label : -1;
     }
 
     this.setState = function(newstate) {
