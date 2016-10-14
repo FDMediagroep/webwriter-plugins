0e4ae34cf922b14f3793e41fa418acab4185c49a
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-10-14 09:49:07 +0200

    WBWRI-537 Updated linkedarticles to always keep links from top position.

--- plugins/linkedarticles/LinkedarticlesComponent.js
+++ plugins/linkedarticles/LinkedarticlesComponent.js
@@ -14,12 +14,7 @@ LinkedarticlesComponent.Prototype = function() {
 
   this.getInitialState = function() {
     var links = this.getLinks();
-    console.log(links)
-
-    return {
-      link1: links._1,
-      link2: links._2
-    };
+    return { link1: links._1, link2: links._2 };
   };
 
   this.render = function() {
@@ -27,70 +22,69 @@ LinkedarticlesComponent.Prototype = function() {
       .append(
         $$('h2').append(this.context.i18n.t('Linked Articles')),
         $$('input')
-          .attr({
-            'type': 'text',
-            'value': this.state.link1,
-            'placeholder': this.context.i18n.t('Related Article 1')
-          })
+          .attr({ 'type': 'text', 'placeholder': this.context.i18n.t('Related Article 1') })
           .ref('link1')
+          .setValue(this.state.link1)
           .on('blur', this.flushLinks.bind(this)),
-          $$('input')
-            .attr({
-              'id': 'linkedarticles-link-2',
-              'type': 'text',
-              'value': this.state.link2,
-              'placeholder': this.context.i18n.t('Related Article 2')
-            })
-            .ref('link2')
-            .on('blur', this.flushLinks.bind(this))
+        $$('input')
+          .attr({ 'type': 'text', 'placeholder': this.context.i18n.t('Related Article 2') })
+          .ref('link2')
+          .setValue(this.state.link2)
+          .on('blur', this.flushLinks.bind(this)),
+        $$('hr')
       );
   };
 
   this.flushLinks = function() {
-    // remove previous link
-    this.context.api
-      .getLinkByType(this.name, 'fdmg/linkedarticles')
-      .forEach(function(link) {
-        this.context.api.removeLinkByUUIDAndRel(
-          this.name,
-          link['@uuid'],
-          link['@rel']);
-      }.bind(this));
 
-    // add new link
-    this.context.api.addLink(this.name, {
-      '@rel': this.name,
-      '@type': 'fdmg/linkedarticles',
-      '@link1': this.refs.link1.val(),
-      '@link2': this.refs.link2.val(),
-      '@uuid': genUUID()
-    });
+    if (this.outOfSync()) {
 
-    this.reloadLinks();
+      // remove previous link
+      this.context.api
+        .getLinkByType(this.name, 'fdmg/linkedarticles')
+        .forEach(function(link) {
+          this.context.api.removeLinkByUUIDAndRel(this.name, link['@uuid'], link['@rel'] );
+        }.bind(this));
+
+      var link1 = this.refs.link1.val();
+      var link2 = this.refs.link2.val();
+
+      // if first is empty and second is set swap links
+      if (!link1 && !!link2) {
+        link1 = link2;
+        link2 = '';
+      }
+
+      // add new link
+      this.context.api.addLink(this.name, {
+        '@rel': this.name,
+        '@type': 'fdmg/linkedarticles',
+        '@link1': link1,
+        '@link2': link2,
+        '@uuid': genUUID()
+      });
+
+      this.reloadLinks();
+    }
   }
 
   this.getLinks = function() {
     return this.context.api
       .getLinkByType(this.name, 'fdmg/linkedarticles')
       .map(function(link) {
-        return {
-          _1: link['@link1'] || '',
-          _2: link['@link2'] || ''
-        };
+        return { _1: link['@link1'] || '', _2: link['@link2'] || '' };
       })
-      .pop() || {
-        _1: '',
-        _2: ''
-      };
+      .pop() || { _1: '', _2: '' };
   };
 
   this.reloadLinks = function() {
     var links = this.getLinks();
-    this.setState({
-      link1: links._1,
-      link2: links._2
-    });
+    this.setState({ link1: links._1, link2: links._2 });
   };
+
+  this.outOfSync = function() {
+    return this.state.link1 != this.refs.link1.val() || this.state.link2 != this.refs.link2.val();
+  }
 };
 
 Component.extend(LinkedarticlesComponent);
