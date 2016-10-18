3fe45c6f48f1717c2cee8441c57ae7694c71ae79
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-10-18 17:14:36 +0200

    WBWRI-204 Added validation for headline and teaser.

--- plugins/fd4validation/Fd4validationValidation.js
+++ plugins/fd4validation/Fd4validationValidation.js
@@ -3,6 +3,8 @@ module.exports = {
   isValid: function(newsItem) {
 
     const pubStatus = this.context.api.getPubStatus()
+    const warnOnDraftErrorOnPublish = getWarnOnDraftErrorOnPublish(pubStatus)
+    const errorOnDraftAndPublish = getErrorOnDraftAndPublish(pubStatus)
     const messages = []
 
     // TODO Text Length
@@ -11,23 +13,58 @@ module.exports = {
     const authors = newsItem.querySelectorAll('itemMeta>links link[type="x-im/author"]')
     if (authors.length < 1) {
       messages.push({
-        type: getMessageType(pubStatus),
+        type: warnOnDraftErrorOnPublish,
         message: this.context.i18n.t('Missing author')
       })
     }
 
-    // TODO Headline
+    // Headline
+    const headline = newsItem.querySelectorAll('idf>group element[type="headline"]')
+    if (headline.length < 1) {
+      messages.push({
+        type: warnOnDraftErrorOnPublish,
+        message: this.context.i18n.t('Missing headline')
+      })
+    }
+    if (headline.length > 1) {
+      messages.push({
+        type: warnOnDraftErrorOnPublish,
+        message: this.context.i18n.t('Too many headlines')
+      })
+    }
 
     // TODO Body+
 
     // TODO Teaser Title
-    // TODO Teaser Body
+    const teaser = newsItem.querySelectorAll('contentMeta>metadata object[type="x-im/teaser"]')
+    if (teaser.length != 1) {
+      messages.push({
+        type: errorOnDraftAndPublish,
+        message: this.context.i18n.t('Missing teaser')
+      })
+    } else {
+      const title = teaser[0].attributes.getNamedItem('title')
+      if (title == null || title.value.trim() == '') {
+        messages.push({
+          type: errorOnDraftAndPublish,
+          message: this.context.i18n.t('Missing teaser title')
+        })
+      }
+
+      const body = teaser[0].querySelectorAll('data>text')
+      if (body.length != 1 || body[0].innerHTML.trim() == '') {
+        messages.push({
+          type: errorOnDraftAndPublish,
+          message: this.context.i18n.t('Missing teaser body')
+        })
+      }
+    }
 
     // Section
     const section = newsItem.querySelectorAll('itemMeta>links link[type="fdmg/section"]')
     if (section.length != 1) {
       messages.push({
-        type: getMessageType(pubStatus),
+        type: warnOnDraftErrorOnPublish,
         message: this.context.i18n.t('Missing section')
       })
     }
@@ -38,7 +75,7 @@ module.exports = {
   }
 }
 
-function getMessageType(pubStatus) {
+function getWarnOnDraftErrorOnPublish(pubStatus) {
   switch (pubStatus.qcode) {
     case 'imext:draft':
     case 'stat:canceled':
@@ -50,4 +87,8 @@ function getMessageType(pubStatus) {
     default:
       throw new Error(`Unknown pubstatus qcode :'${pubStatus.qcode}'`)
   }
+}
+
+function getErrorOnDraftAndPublish(pubStatus) {
+  return 'error'
 }
