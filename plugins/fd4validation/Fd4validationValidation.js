fb2653c1ba6285ffcb8c50d4775e2d2ce14396c5
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-11-02 14:09:10 +0100

    WBWRI-204 Refactored validation rules for better readability.

--- plugins/fd4validation/Fd4validationValidation.js
+++ plugins/fd4validation/Fd4validationValidation.js
@@ -7,6 +7,98 @@
 
 */
 
+const $ = require('substance/util/jquery')
+
+const emptyBody = (x) => x.length == 0 || !x[0] || x[0].innerHTML.trim() == ''
+const emptyTitle = (x) => x.length == 0 || !x[0] || x[0].value.trim() == ''
+const moreThanOne = (x) => x.length > 1
+const lessThanOne = (x) => x.length < 1
+const exactlyOne = (x) => x.length == 1
+const isUnderRange = (x) => x[0].classList.contains('under-range')
+const isOverRange = (x) => x[0].classList.contains('over-range')
+const atLeastOne = (x) => x.length >= 1
+const isValidFd4Url = (x) => (/^.*fd\.nl.*\/(\d+).*$/i).test(x)
+
+module.exports = {
+  isValid: function(newsItem) {
+
+    const acc = (function() {
+      const messages = []
+      return {
+        addWarning: function(message) {
+          messages.push({message, type: 'warning'})
+        },
+        addError: function(message) {
+          messages.push({message, type: 'error'})
+        },
+        read: function() {
+          return messages
+        }
+      }
+    })()
+
+    const publicationStatus = (function(pubStatus) {
+      switch (pubStatus.qcode) {
+        case 'imext:draft':   return 'draft'
+        case 'stat:usable':   return 'published'
+        case 'stat:withheld': // fallthrough
+        case 'imext:done':    return 'submitted'
+        default:
+          console.log(`Unknown pubstatus qcode: ${pubStatus.qcode}`)
+          return 'foobar'
+      }
+    })(this.context.api.getPubStatus())
+
+    const drafting = publicationStatus == 'draft'
+    const publishing = publicationStatus == 'published'
+    const submitting = publicationStatus == 'submitted'
+
+    const headline = newsItem.querySelectorAll('idf>group element[type="headline"]')
+    const author = newsItem.querySelectorAll('itemMeta>links link[type="x-im/author"]')
+    const teaser = newsItem.querySelectorAll('contentMeta>metadata object[type="x-im/teaser"]')
+    const teasertitle = teaser.length > 0 ? [teaser[0].attributes.getNamedItem('title')] || [] : []
+    const teaserbody = teaser.length > 0 ? teaser[0].querySelectorAll('data>text') : []
+    const textcount = newsItem.querySelectorAll('itemMeta>links link[type="fdmg/textcount"]')
+    const charcount = $('#fd4validation-character-count')
+    const section = newsItem.querySelectorAll('itemMeta>links link[type="fdmg/section"]')
+    const tags = newsItem.querySelectorAll('itemMeta>links link[type="x-im/category"]')
+
+    const relatedarticles = Array.from(newsItem.querySelectorAll('itemMeta>links link[type="fdmg/relatedarticle"]').values())
+      .map((x) => x.attributes.getNamedItem('url'))
+      .filter((x) => !!x)
+      .map((x) => x.value)
+
+    if (emptyBody(headline) && (submitting || publishing)) acc.addError('Missing headline')
+    if (moreThanOne(headline) && (submitting || publishing)) acc.addError('More than one headline')
+
+    if (lessThanOne(author)) acc.addError('Missing author')
+
+    if (!exactlyOne(teaser)) acc.addError('Missing teaser block')
+    if (moreThanOne(teaser)) acc.addError('Too many teaser blocks')
+    if (exactlyOne(teaser) && emptyTitle(teasertitle) && publishing) acc.addError('Missing teaser title')
+    if (exactlyOne(teaser) && emptyBody(teaserbody) && publishing) acc.addError('Missing teaser body')
+
+    if (!exactlyOne(textcount) && publishing) acc.addError('Missing text length')
+    if (exactlyOne(textcount) && exactlyOne(charcount) && isUnderRange(charcount) && (submitting || publishing)) acc.addWarning('Not enough characters')
+    if (exactlyOne(textcount) && exactlyOne(charcount) && isOverRange(charcount) && (submitting || publishing)) acc.addWarning('Too many characters')
+    //
+    if (!exactlyOne(section)) acc.addError('Missing section')
+    //
+    if (lessThanOne(tags) && publishing) acc.addError('Missings tags')
+
+    // TODO Check related articles by DOM id instead of NewsML
+    if (atLeastOne(relatedarticles) && !relatedarticles.every(isValidFd4Url) && (drafting || submitting)) acc.addWarning('Invalid related article url')
+    if (atLeastOne(relatedarticles) && !relatedarticles.every(isValidFd4Url) && publishing) acc.addWarning('Invalid related article url')
+
+    // TODO Redirect article valid url
+
+    return acc.read()
+  }
+}
+
+
+
+/*
 const $ = require('substance/util/jquery');
 
 module.exports = {
@@ -126,3 +218,4 @@ function MessageAccumulator() {
     }
   }
 }
+*/
