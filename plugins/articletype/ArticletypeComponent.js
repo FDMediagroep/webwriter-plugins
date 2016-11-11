23f1036494615fc7a9aa7d8f7d83894d60c33f9a
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-11-11 15:21:43 +0100

    Boyscouting: Removed obsolete Validatable and Linkselector plugins.

--- plugins/articletype/ArticletypeComponent.js
+++ plugins/articletype/ArticletypeComponent.js
@@ -82,8 +82,8 @@ ArticletypeComponent.Prototype = function() {
           },
           header: this.context.i18n.t('Article type'),
           items: this.state.items,
-          allowFreeInput: true,
-          allowEmptySelection: true,
+          allowFreeInput: false,
+          allowEmptySelection: false,
           initialSelection: initialSelection,
           disabled: !this.state.enabled
         }),
@@ -107,73 +107,3 @@ ArticletypeComponent.Prototype = function() {
 
 Component.extend(ArticletypeComponent)
 module.exports = ArticletypeComponent
-
-// 'use strict';
-//
-// var LinkSelectorComponent = require('vendor/nl.fdmg/linkselector/LinkSelectorComponent');
-//
-// var __articletype_state;
-//
-// function ArticletypeComponent() { LinkSelectorComponent.apply(this, arguments); }
-//
-// ArticletypeComponent.Prototype = function() {
-//     this.getInitialState = function() { return __articletype_state || LinkSelectorComponent.prototype.getInitialState.call(this); }
-//
-//     this.initialize = function() {
-//         if (this.getState()['initialized?']) return;
-//
-//         this.extendState({
-//             'initialized?': true,
-//             'pluginname': 'articletype',
-//             'plugintype': 'fdmg/articletype',
-//             'heading': this.context.i18n.t('Article type'),
-//             'forcelistmode?': true,
-//         });
-//
-//         var endpoint = this.context.api.getConfigValue('articletype', 'endpoint');
-//
-//         this.loadList(endpoint, function(items) {
-//             items = this
-//                 .sortByAplhabet(items, 'title')
-//                 .map(function(item) { return { 'id': item.name, 'label': item.title } });
-//             return items;
-//         }.bind(this));
-//
-//         // check to see if we are disabled because of a redirectlink
-//         var redirectlink = this.context.api.getLinkByType('redirectlink', 'fdmg/redirectlink').pop();
-//         if (!!redirectlink && redirectlink['@checked'] === 'true') this.disableComponent();
-//     }
-//
-//     this.didMount = function() {
-//         this.context.api.on('articletype', 'articletype:enable', this.enableComponent.bind(this));
-//         this.context.api.on('articletype', 'articletype:disable', this.disableComponent.bind(this));
-//     }
-//
-//     this.dispose = function() {
-//         // remove any existing listeners first, dispatcher only allows one registry
-//         this.context.api.off('articletype', 'articletype:enable');
-//         this.context.api.off('articletype', 'articletype:disable');
-//     }
-//
-//      // Override this so that we can get the correct label
-//     this.getExistingLinkOrDefault = function(name, type, prop) {
-//         var links = this.context.api.getLinkByType(name, type);
-//         if (links.length < 1) {
-//           return this.state.emptyitem;
-//         } else {
-//           var id = links.shift()[prop];
-//           var label = this.getItemLabelById(id);
-//
-//           return { 'id': id, 'label': label };
-//         }
-//     }
-//
-//     this.setState = function(newstate) {
-//         __articletype_state = newstate;
-//         LinkSelectorComponent.prototype.setState.call(this, newstate);
-//     }
-// }
-//
-// LinkSelectorComponent.extend(ArticletypeComponent);
-//
-// module.exports = ArticletypeComponent;
