ca54c5ad37785c974e36fdf6b451392e8494f7e9
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-11-22 16:42:45 +0100

    WBWRI-672 Added unknown field to text count.

--- plugins/textcount/TextcountMainComponent.js
+++ plugins/textcount/TextcountMainComponent.js
@@ -1,218 +1,178 @@
-'use strict';
-
-var Component = require('substance/ui/Component');
-var $$ = Component.$$;
-var jQuery = require('substance/util/jquery');
-var _ = require('lodash');
-var genUUID = require('writer/utils/IdGenerator');
+const Component = require('substance/ui/Component')
+const $$ = Component.$$
+const genUuid = require('writer/utils/IdGenerator')
+const _ = require('lodash')
 
 function TextcountMainComponent() {
-    TextcountMainComponent.super.apply(this, arguments);
+  TextcountMainComponent.super.apply(this, arguments)
 
-    this.context.api.on('wordcounter', 'document:changed', function () {
-        this.onDocumentChanged();
-    }.bind(this));
+  this.context.api.on('textcount', 'document:changed', function() {
+    this.updateCount()
+  }.bind(this))
 }
 
-TextcountMainComponent.Prototype = function () {
+TextcountMainComponent.Prototype = function() {
 
-    this.dispose = function() {
-        this.context.api.off('wordcounter', 'document:changed');
-    };
+  this.getInitialState = function() {
+    const countedElements = this.context.api.getConfigValue('textcount', 'countedElements', [])
+    const availableSizes = this.context.api.getConfigValue('textcount', 'sizes', [])
+    const margin = this.context.api.getConfigValue('textcount', 'marginPercentage')
+    const documentSize = this.getDocumentSize()
+    const wordCount = 0
+    const charCount = 0
 
-    // Get the word- and character count.
-    this.getCount = function () {
-        var nodes = this.context.api.getDocumentNodes();
-        var config = this.getConfiguration();
-        var countedElements = config.countedElements;
+    return {
+      countedElements,
+      availableSizes: ([{size: '?', charCount: '?', disabled: true}]).concat(availableSizes),
+      margin,
+      documentSize,
+      wordCount,
+      charCount
+    }
+  }
 
-        var textContent = "";
+  this.didMount = function() {
+    this.updateCount();
+  }
 
-        nodes.forEach(function (node) {
+  this.render = function() {
+    return $$('div')
+      .addClass('textcount plugin')
+      .append(
+        $$('hr'),
+        $$('div')
+          .addClass('count__info-container fdmg-sidebar')
+          .append(
+            $$('div')
+              .addClass('count-info form-group')
+              .append(
+                $$('select')
+                  .ref('select')
+                  .append(
+                    this.state.availableSizes.map((size) => {
+                      const label = size.size
+                      const charCount = (size.charCount == 'Inf' ? '∞' : size.charCount)
+                      return $$('option')
+                        .attr({
+                          'data-id': label,
+                          disabled: size.disabled,
+                          selected: size.disabled || this.state.documentSize == label
+                        })
+                        .append(size.charCount != '?' ? `${label} ( ${charCount} )` : label)
+                    })
+                  )
+                  .on('change', function(e) {
+                    const size = this.refs.select.$el.find('option:selected').data('id')
+                    this.updateDocumentSize(size)
+                  }.bind(this)),
+                $$('p').append(this.context.i18n.t('Size'))
+            ),
+            $$('div')
+              .addClass('count-info')
+              .attr({title: this.context.i18n.t('Character count')})
+              .append(
+                $$('span').addClass(this.getCharCountModifier()).append('' + this.state.charCount),
+                $$('p').append(this.context.i18n.t('Characters'))
+              ),
+            $$('div')
+              .addClass('count-info')
+              .attr({title: this.context.i18n.t('Word count')})
+              .append(
+                $$('span').append('' + this.state.wordCount),
+                $$('p').append(this.context.i18n.t('Words'))
+              )
+        )
+      )
+  }
 
-        	if (countedElements.some(function(c) { return node.isInstanceOf(c); })) {
+  this.dispose = function() {
+    this.context.api.off('textcount', 'document:changed')
+  }
 
-	            if (node.content) {
-	                textContent += node.content.trim();
-	            }
-        	}
+  this.updateCount = function() {
+    const count = this.performCount()
 
-        });
+    this.extendState({
+      wordCount: count.words,
+      charCount: count.chars
+    })
+  }
 
-        var words = textContent.split(/\s+/);
-        var textLength = textContent.length;
+  this.getDocumentSize = function() {
+    const links = this.context.api.getLinkByType('textcount', 'fdmg/textcount')
 
-        return {
-            words: words.length,
-            textLength: textLength
-        };
-    };
+    if (links.length >= 1) {
+      return links[0]['@size']
+    }
+    return ''
+  }
 
-    this.getConfiguration = function () {
-    	// Get plugins/nodes to count as defined in the configuration.
-    	var countedElements = this.context.api.getConfigValue("textcount", "countedElements", []);
+  this.updateDocumentSize = function(size) {
+    const api = this.context.api
 
-        // Get the available sizes.
-        var availableSizes = this.context.api.getConfigValue("textcount", "sizes");
+    // Delete previous document size(s)
+    api.getLinkByType('textcount', 'fdmg/textcount').forEach((l) => api.removeLinkByUUIDAndRel('textcount', l['@uuid'], l['@rel']))
 
-        // Set an extra value to work with when there is no link or config present.
-        availableSizes["vrije length"] = Number.MAX_VALUE;
+    // Write size to document
+    api.addLink('textcount', {
+      '@rel': 'textcount',
+      '@type': 'fdmg/textcount',
+      '@size': size,
+      '@uuid': genUuid()
+    })
 
-        // Get the character margin (in percentages).
-        var marginPercentage = this.context.api.getConfigValue("textcount", "marginPercentage");
+    // Update component state
+    this.extendState({documentSize: size})
+  }
 
-        // get the defined size in the article's link (S, L, XL, XXL). If there is no link fall back
-        // to "?" which represents MAX_VALUE.
-        var links = this.context.api.getLinkByType("textcount", "fdmg/textcount");
+  this.performCount = function() {
+    const nodes = this.context.api.getDocumentNodes()
+    const countedElements = this.state.countedElements
 
-        // Get the corresponding character count from the plugin configuration.
-        var documentSize = links.length > 0 ? links.pop()["@size"] : "vrije length";
-        var targetSize = availableSizes[documentSize];
+    const words = _(nodes)
+      .filter((node) => countedElements.some((el) => node.isInstanceOf(el)))
+      .filter((node) => !!node.content)
+      .map((node) => node.content.trim())
+      .flatMap((paragraph) => paragraph.split(/\s+/))
 
-        return {
-            availableSizes: availableSizes,
-            marginPercentage: marginPercentage,
-            targetSize: targetSize,
-            countedElements: countedElements
-        };
+    const charsCount = words.reduce((acc, word) => acc + word.length, 0)
+
+    return {
+      words: words.size(),
+      chars: charsCount
+    }
+  }
+
+  this.getCharCountModifier = function() {
+    const target = this.getDocumentTargetCharCount()
+    const actual = this.state.charCount
+    const margin = target / 100 * parseFloat(this.state.margin)
+    const min = target - margin
+    const max = target + margin
+
+    switch (true) {
+      case (target == -1):                    return ''
+      case (actual == target):                return 'perfect-range'
+      case (actual < min):                    return 'under-range'
+      case (actual > max):                    return 'over-range'
+      case (actual >= min && actual <= max ): return 'in-range'
+      default:                                return ''
+    }
+  }
+
+  this.getDocumentTargetCharCount = function() {
+    const availableSizes = this.state.availableSizes
+    const documentSize = this.state.documentSize
+    const maybeCharCount = availableSizes.find((size) => size.size == documentSize)
+
+    if (maybeCharCount != undefined) {
+      const charCount = parseInt(maybeCharCount.charCount)
+      if (!Number.isNaN(charCount)) return charCount
     }
 
-    this.onDocumentChanged = function () {
-        var count = this.getCount();
+    return -1
+  }
+}
 
-        this.extendState({
-            textLength: count.textLength,
-            words: count.words
-        });
-    };
-
-    this.setArticleSize = function () {
-
-        var api = this.context.api;
-        var name = "textcount"
-
-        // Remove all existing links.
-        api.getLinkByType(name, "fdmg/textcount")
-            .forEach(function(link) {
-                api.removeLinkByUUIDAndRel(name, link['@uuid'], link['@rel']);
-            });
-
-        // What is the current selected size from the dropdown.
-        var selected = jQuery('.textcount select option:selected');
-        var selectedSize = selected.data("id");
-
-        // Add a new link with a newly selected size.
-        api.addLink(name, {
-            '@rel': 'textcount',
-            '@size': selectedSize,
-            '@type': 'fdmg/textcount',
-            '@uuid': genUUID()
-        });
-
-        // Get the current size in the state and replace it with the
-        // newly selected size.
-        var currentSize = this.state.availableSizes;
-        var newSize = currentSize[selectedSize];
-
-        // Update state with new size.
-        this.extendState({ targetSize : newSize })
-    };
-
-    this.getInitialState = function () {
-        var config = this.getConfiguration();
-        var count = this.getCount();
-
-        return {
-            textLength: count.textLength,
-            words: count.words,
-            availableSizes: config.availableSizes,
-            marginPercentage: config.marginPercentage,
-            targetSize: config.targetSize,
-            countedElements: config.countedElements
-        };
-    };
-
-    this.render = function () {
-        countClass = "";
-        var countClass = calculateCountClass.bind(this)();
-        var availableSizes = this.state.availableSizes
-
-        var el = $$('div').addClass('textcount plugin')
-            .append(
-                [
-                    $$('h2'),
-                    $$('hr')
-                ]);
-
-        var numberContainer = $$('div').addClass('count__info-container fdmg-sidebar');
-
-        // Populate articleSizeSelect with key and value like: S (1810).
-        var articleSizeSelect = $$('div').addClass('count-info form-group')
-            .append(
-                $$('select')
-                    .append(Object.keys(availableSizes).map(function(key){
-                        var value = availableSizes[key];
-
-                        return $$('option')
-                            .append(key + " " + (key == 'vrije length' ? '' : value))
-                            .attr({
-                                "data-id" : key,
-                                "selected" : this.state.targetSize == value
-                            })
-                    }.bind(this)))
-                )
-            .on('change', function(){
-                this.setArticleSize();
-            })
-            .append(
-                $$('p')
-                    .append(this.context.i18n.t('Size'))
-            );
-
-        var textlengthEl = $$('div').addClass("count-info")
-            .append($$('span').addClass(countClass).append(this.state.textLength.toString()).attr('id', 'fd4validation-character-count'))
-            .append($$('p').append(this.context.i18n.t('Characters')))
-            .attr('title', this.context.i18n.t('Character count'));
-
-        var wordsEl = $$('div').addClass("count-info")
-            .append($$('span').append(this.state.words.toString()))
-            .append($$('p').append(this.context.i18n.t('Words')))
-            .attr('title', this.context.i18n.t('Word count'));
-
-        numberContainer.append([
-            articleSizeSelect,
-            textlengthEl,
-            wordsEl
-        ]);
-        el.append(numberContainer);
-
-        return el;
-
-        function calculateCountClass(){
-
-            // Keep track of article margin and return the
-            // appropriate class to show the status.
-            var targetSize = this.state.targetSize;
-            var marginPercentage = this.state.marginPercentage;
-            var textLength = this.state.textLength;
-
-            var characterMargin = (targetSize / 100) * marginPercentage;
-            var minCharacters = targetSize - characterMargin;
-            var maxCharacters = targetSize + characterMargin;
-
-            if (textLength > maxCharacters) {
-                return "over-range";
-            } else if (textLength === targetSize) {
-                return "perfect-range";
-            } else if (textLength > minCharacters && textLength < maxCharacters || textLength == maxCharacters ) {
-                return "in-range";
-            } else if (textLength < minCharacters && targetSize != this.getConfiguration().availableSizes['vrije length']) {
-              return "under-range";
-            } else {
-              return "";
-            }
-        }
-    };
-};
 Component.extend(TextcountMainComponent);
 module.exports = TextcountMainComponent;
