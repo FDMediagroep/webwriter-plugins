0628b49fddb77a1abe79b1791d9721d60652cb57
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2016-12-01 14:35:42 +0100

    Replaced showrelatedartices, removed old fdpersonal and donotindex plugins

--- plugins/index.js
+++ plugins/index.js
@@ -1,7 +1,9 @@
 import DoNotIndex from './nl.fdmg.donotindex/index'
 import FDPersonal from './nl.fdmg.fdpersonal/index'
+import ShowRelatedArticles from './nl.fdmg.showrelatedarticles/index'
 
 (() => {
   DoNotIndex()
   FDPersonal()
+  ShowRelatedArticles()
 })()
