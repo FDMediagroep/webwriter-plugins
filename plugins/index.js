f29ced0d126e8c1065565639ced11a467b9a5d24
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-12-01 16:19:44 +0100

    Replaced section plugin.

--- plugins/index.js
+++ plugins/index.js
@@ -2,8 +2,12 @@ import DoNotIndex from './nl.fdmg.donotindex/index'
 import FDPersonal from './nl.fdmg.fdpersonal/index'
 import ShowRelatedArticles from './nl.fdmg.showrelatedarticles/index'
 
+import Section from './nl.fdmg.section/index'
+
 (() => {
   DoNotIndex()
   FDPersonal()
   ShowRelatedArticles()
+
+  Section()
 })()
---
+++ plugins/nl.fdmg.section/index.js
@@ -0,0 +1,10 @@
+import SectionPackage from './SectionPackage'
+import {registerPlugin} from 'writer'
+
+export default () => {
+  if (registerPlugin) {
+    registerPlugin(SectionPackage)
+  } else {
+    console.error("Register method not yet available");
+  }
+}
