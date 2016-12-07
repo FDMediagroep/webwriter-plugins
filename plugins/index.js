3730bbef0005276509a3744b00fe885fb9332578
Willem Liu <willem.liu@fdmediagroep.nl>
2016-12-07 11:56:45 +0100

    WBWRI-682 WIP Re-added numberframe. Lost during merge.

--- plugins/index.js
+++ plugins/index.js
@@ -1,16 +1,16 @@
 import TextAnalyzer from './nl.fdmg.textanalyzer/index';
 
-import Author from './nl.fdmg.author/index'
-import Tags from './nl.fdmg.tags/index'
+import Author from './nl.fdmg.author/index';
+import Tags from './nl.fdmg.tags/index';
 
-import DoNotIndex from './nl.fdmg.donotindex/index'
-import FDPersonal from './nl.fdmg.fdpersonal/index'
-import ShowRelatedArticles from './nl.fdmg.showrelatedarticles/index'
-import Paywall from './nl.fdmg.paywall/index'
-import Topstory from './nl.fdmg.topstory/index'
-import Comments from './nl.fdmg.comments/index'
-import Breakingarticle from './nl.fdmg.breakingarticle/index'
-import Shortarticle from './nl.fdmg.shortarticle/index'
+import DoNotIndex from './nl.fdmg.donotindex/index';
+import FDPersonal from './nl.fdmg.fdpersonal/index';
+import ShowRelatedArticles from './nl.fdmg.showrelatedarticles/index';
+import Paywall from './nl.fdmg.paywall/index';
+import Topstory from './nl.fdmg.topstory/index';
+import Comments from './nl.fdmg.comments/index';
+import Breakingarticle from './nl.fdmg.breakingarticle/index';
+import Shortarticle from './nl.fdmg.shortarticle/index';
 
 import RelatedArticles from './nl.fdmg.relatedarticles/index';
 
@@ -18,42 +18,44 @@ import Section from './nl.fdmg.section/index';
 import Genre from './nl.fdmg.genre/index';
 import Rubric from './nl.fdmg.rubric/index';
 
-import ArticleType from './nl.fdmg.articletype/index'
-import RedirectLink from './nl.fdmg.redirectlink/index'
-import Epic from './nl.fdmg.epic/index'
+import ArticleType from './nl.fdmg.articletype/index';
+import RedirectLink from './nl.fdmg.redirectlink/index';
+import Epic from './nl.fdmg.epic/index';
 
-import TextcountSelector from './nl.fdmg.textcountselector/index'
+import TextcountSelector from './nl.fdmg.textcountselector/index';
 
-import Quote from './nl.fdmg.quote/index'
-import Stockticker from './nl.fdmg.stockticker/index'
+import Quote from './nl.fdmg.quote/index';
+import NumberFrame from './nl.fdmg.numberframe/index';
+//import Stockticker from './nl.fdmg.stockticker/index';
 
 (() => {
-  TextAnalyzer()
+  TextAnalyzer();
 
-  Author()
-  Tags()
+  Author();
+  Tags();
 
-  DoNotIndex()
-  FDPersonal()
-  ShowRelatedArticles()
-  Paywall()
-  Topstory()
-  Comments()
-  Breakingarticle()
-  Shortarticle()
+  DoNotIndex();
+  FDPersonal();
+  ShowRelatedArticles();
+  Paywall();
+  Topstory();
+  Comments();
+  Breakingarticle();
+  Shortarticle();
 
-  Section()
-  Genre()
-  Rubric()
+  Section();
+  Genre();
+  Rubric();
 
-  RelatedArticles()
+  RelatedArticles();
 
-  ArticleType()
-  RedirectLink()
-  Epic()
+  ArticleType();
+  RedirectLink();
+  Epic();
 
-  TextcountSelector()
+  TextcountSelector();
 
-  Quote()
-  Stockticker()
+  NumberFrame();
+  Quote();
+  //Stockticker();
 })()
