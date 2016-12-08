844dece4aa7936e5cfe9f37947336873c10dc4b4
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-12-08 16:08:56 +0100

    Updated writer config to use S3 plugin repo.

--- s3Upload.js
+++ s3Upload.js
@@ -5,8 +5,8 @@ const path = require('path')
 const bucketName = process.env.AWS_S3_BUCKET_NAME
 
 if(!bucketName) {
-    throw new Error("No bucket specified")
-    process.exitCode(1)
+  throw new Error("No bucket specified")
+  process.exitCode(1)
 }
 
 const distFolder = path.join(__dirname, "dist")
@@ -18,11 +18,11 @@ const distFolder = path.join(__dirname, "dist")
  * @returns {*}
  */
 function getContentTypeForExtension(extension) {
-    if (extension === 'css') {
-        return 'text/css'
-    } else if (extension === 'js') {
-        return 'application/javascript'
-    }
+  if (extension === 'css') {
+    return 'text/css'
+  } else if (extension === 'js') {
+    return 'application/javascript'
+  }
 }
 
 
@@ -32,41 +32,39 @@ function getContentTypeForExtension(extension) {
  */
 fs.readdir(distFolder, (err, files) => {
 
-    files
-        .map((file) => {
-            return path.join(distFolder, file);
+  files
+    .map((file) => {
+      return path.join(distFolder, file);
+    })
+    .filter((file) => {
+      return fs.statSync(file).isFile();
+    })
+    .forEach((file) => {
+      const fileName = path.basename(file)
+      const fileType = path.extname(file).split('.').pop()
+
+      let fileStreamJs = fs.createReadStream(file);
+      fileStreamJs.on('error', function (err) {
+        if (err) {
+          throw err;
+        }
+      })
+
+      let s3 = new aws.S3();
+
+      let uploadFileObject = {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileStreamJs,
+        ContentType: getContentTypeForExtension(fileType)
+      }
+
+      fileStreamJs.on('open', () => {
+        s3.putObject(uploadFileObject, (err) => {
+          if (err) {
+            throw err;
+          }
         })
-        .filter((file) => {
-            return fs.statSync(file).isFile();
-        })
-        .forEach((file) => {
-            const fileName = path.basename(file)
-            const fileType = path.extname(file).split('.').pop()
-
-            let fileStreamJs = fs.createReadStream(file);
-            fileStreamJs.on('error', function (err) {
-                if (err) {
-                    throw err;
-                }
-            })
-
-            let s3 = new aws.S3();
-
-            let uploadFileObject = {
-                Bucket: bucketName,
-                Key: fileName,
-                Body: fileStreamJs,
-                ContentType: getContentTypeForExtension(fileType)
-            }
-
-            fileStreamJs.on('open', () => {
-                s3.putObject(uploadFileObject, (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                })
-            })
-
-        })
-
+      })
+    })
 })
