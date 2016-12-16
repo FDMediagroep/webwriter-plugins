a970a027e3df05ec9a1aff225bce548903d91fc7
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-12-16 11:56:05 +0100

    Added edit modal title translation to work instructions.

--- plugins/nl.fdmg.workinstructions/WorkInstructionsPackage.js
+++ plugins/nl.fdmg.workinstructions/WorkInstructionsPackage.js
@@ -6,6 +6,9 @@ export default {
   name: 'workinstructions',
   configure: function (config) {
     config.addComponentToSidebarWithTabId(this.id, 'main', WorkInstructionsComponent)
+    config.addLabel('Edit workinstructions', {
+      'nl': 'Werkinstructies aanpassen'
+    })
     config.addLabel('Workinstructions placeholder', {
       'nl' : 'b.v. locatie en opmerkingen'
     })
