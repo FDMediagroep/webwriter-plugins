7d5647c6b04dab6297a49d66ea9b7fdd864f0aa8
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-12-16 16:53:27 +0100

    Added validation for text count selector.

--- plugins/nl.fdmg.textcountselector/TextcountSelectorPackage.js
+++ plugins/nl.fdmg.textcountselector/TextcountSelectorPackage.js
@@ -1,12 +1,17 @@
 import TextcountSelectorComponent from './TextcountSelectorComponent'
+import TextcountSelectorValidator from './TextcountSelectorValidator'
 
 export default {
   id: 'nl.fdmg.textcountselector',
   name: 'textcountselector',
   configure: function(config) {
     config.addComponentToSidebarWithTabId(this.id, 'adminTab', TextcountSelectorComponent)
+    config.addValidator(TextcountSelectorValidator)
     config.addLabel('Textcount size', {
       'nl': 'Tekstlengte (karakters)'
+    })
+    config.addLabel('Missing text length', {
+      'nl': 'Tekstlengte is niet ingesteld'
     })
   }
 }
