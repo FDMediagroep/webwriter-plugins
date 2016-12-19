7a9f3856870a4f582f8c0d78a66686799df68d65
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2016-12-19 15:19:24 +0100

    WBWRI-682 The article- prefix is now removed from tag id when storing tags to article endpoint

--- plugins/nl.fdmg.tags/TagsComponent.js
+++ plugins/nl.fdmg.tags/TagsComponent.js
@@ -105,9 +105,11 @@ class TagsComponent extends Component {
   _saveTagListAsync() {
     const endpoint = api.getConfigValue(pluginId, 'updateEndpoint')
     const token = api.getConfigValue(pluginId, 'token')
-    const id = api.newsItem.getGuid()
+    const id = api.newsItem.getGuid().replace(/article-/,'');
+    
     const url = endpoint + id
     const body = JSON.stringify(this.state.existingTags.map(tag => tag.name))
+    console.log(url, id)
 
     fetch(url, {
       method: 'PUT',
