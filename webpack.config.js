6ecad9bf855e2c64faa1563cf75741f9b99da27f
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-12-20 16:33:47 +0100

    Updated code styles of webpack configs.

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,8 @@
 |
 */
 
-const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 console.log("\n ----------------------------")
@@ -19,66 +17,66 @@ console.log(" Plugin development build ")
 console.log(" ----------------------------\n")
 
 module.exports = {
-    entry: "./plugins/index.js",
-    output: {
-        filename: "index.js",
-        path: "dist",
-    },
-    devServer: {
-      historyApiFallback: true,
-      inline: true,
-      compress: false,
-      port: 3000
-    },
-    externals: {
-        writer: 'writer',
-        substance: 'substance'
-    },
-    devtool: 'source-map',
-    postcss: [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        })
-    ],
-    module: {
+  entry: "./plugins/index.js",
+  output: {
+    filename: "index.js",
+    path: "dist",
+  },
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    compress: false,
+    port: 3000
+  },
+  externals: {
+    writer: 'writer',
+    substance: 'substance'
+  },
+  devtool: 'source-map',
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass')
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
         loaders: [
-          {
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-            loader: 'url-loader?limit=100000'
-          },
-          {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract('style', 'css!sass')
-          },
-          {
-              test: /\.jsx?$/,
-              exclude: /(node_modules)/,
-              loaders: [
-                  'babel?presets[]=stage-0,presets[]=es2015-node6'
-              ]
-          }
-        ],
-        preLoaders: [
-          { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ }
+          'babel?presets[]=stage-0,presets[]=es2015-node6'
         ]
-    },
-    cssLoader: {
-        // True enables local scoped css
-        modules: false,
-        // Which loaders should be applied to @imported resources (How many after the css loader)
-        importLoaders: 1,
-        sourceMap: true
-    },
-    eslint: {
-        failOnWarning: false,
-        failOnError: false
-    },
-    plugins: [
-        new ExtractTextPlugin("style.css"),
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify('development')
-          }
-        })
+      }
+    ],
+    preLoaders: [
+        { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ }
     ]
+  },
+  cssLoader: {
+    // True enables local scoped css
+    modules: false,
+    // Which loaders should be applied to @imported resources (How many after the css loader)
+    importLoaders: 1,
+    sourceMap: true
+  },
+  eslint: {
+    failOnWarning: false,
+    failOnError: false
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ]
 };
