c14d4ff58b45a17022631cbebfde76827cbccd13
Willem Liu <willem.liu@fdmediagroep.nl>
2016-12-21 22:16:53 +0100

    CI: writer-fd-dev.json is changed back to the base file which point to plugins on localhost:3000. That file is transformed during deployment for its specific environment and subsequently uploaded to the Informaker S3 bucket.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,42 @@ var gutil = require('gulp-util');
 var gzip = require("gulp-gzip");
 var print = require('gulp-print');
 var s3 = require("gulp-s3");
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+
+gulp.task('dev-config-generate', function(){
+  gulp.src(['writer-fd-dev.json'])
+    .pipe(replace(/localhost:3000/g, 's3-eu-west-1.amazonaws.com/webwriter-dev-plugins'))
+    .pipe(rename('dev-writer-client.json'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('acc-config-generate', function(){
+  gulp.src(['writer-fd-dev.json'])
+    .pipe(replace(/localhost:3000/g, 's3-eu-west-1.amazonaws.com/webwriter-acc-plugins'))
+    .pipe(rename('acc-writer-client.json'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('prod-config-generate', function(){
+  gulp.src(['writer-fd-dev.json'])
+    .pipe(replace(/localhost:3000/g, 's3-eu-west-1.amazonaws.com/webwriter-plugins'))
+    .pipe(rename('prod-writer-client.json'))
+    .pipe(gulp.dest('./dist'));
+});
+
+function s3ConfigDeploy(configFile) {
+  var config = {
+    "key": gutil.env.aws_key,
+    "secret": gutil.env.aws_secret,
+    "bucket": gutil.env.aws_bucket,
+    "region": gutil.env.aws_region
+  };
+  var options = { headers: {'x-amz-acl': 'bucket-owner-read'} };
+  return gulp.src(configFile)
+    .pipe(print())
+    .pipe(s3(config, options));
+}
 
 /**
  * Deploy to S3 using environment variables.
@@ -21,6 +57,19 @@ gulp.task('deploy', [], function() {
     .pipe(print())
     .pipe(gzip())
     .pipe(s3(config, { gzippedOnly: true }));
+});
+
+/**
+ * Deploy webwriter configuration to InfoMaker S3 bucket using environment variables.
+ */
+gulp.task('dev-deploy-webwriter-config', ['dev-config-generate'], function() {
+  return s3ConfigDeploy('./dist/dev-writer-client.json');
+});
+gulp.task('acc-deploy-webwriter-config', ['acc-config-generate'], function() {
+  return s3ConfigDeploy('./dist/acc-writer-client.json');
+});
+gulp.task('prod-deploy-webwriter-config', ['prod-config-generate'], function() {
+  return s3ConfigDeploy('./dist/prod-writer-client.json');
 });
 
 // Default build development
