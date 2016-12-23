479ca3bef9ce4c50ae049e79701229913ebd177d
Kay Kreuning <kay.kreuning@fdmediagroep.nl>
2016-12-23 14:30:24 +0100

    Removed dead code from textframe plugin.

--- plugins/nl.fdmg.textframe/TextframeInsertImageCommand.js
+++ plugins/nl.fdmg.textframe/TextframeInsertImageCommand.js
@@ -1,4 +1,4 @@
-import {WriterCommand, api, idGenerator} from 'writer'
+import {WriterCommand, api} from 'writer'
 
 class TextframeInsertImageCommand extends WriterCommand {
 
@@ -11,7 +11,6 @@ class TextframeInsertImageCommand extends WriterCommand {
 
     const textframeNode = params.context.node
     const editorSession = context.editorSession
-    const doc = editorSession.getDocument()
     const file = params.data[0] // Textframe only supports one image, take the first one
 
     editorSession.transaction((tx) => {
