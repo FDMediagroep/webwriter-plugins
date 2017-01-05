96a4e0f4155e6aa89ee035d7311ab511859f5fe5
Willem Liu <willem.liu@fdmediagroep.nl>
2017-01-05 09:27:31 +0100

    WBWRI-841 Quote plugin now also uses the selected text as quote.

--- plugins/nl.fdmg.quote/QuoteCommand.js
+++ plugins/nl.fdmg.quote/QuoteCommand.js
@@ -7,9 +7,31 @@ export default class QuoteCommand extends Command {
     };
   }
 
+  /**
+   * Join the selection of text in the writer and return it.
+   * @param selection
+   * @returns {string}
+   */
+  getSelectionText(selection) {
+
+    console.log(selection.getFragments());
+    let joinedText = '';
+    selection.getFragments().forEach(fragment => {
+      if(fragment.type === 'selection-fragment') {
+        joinedText += document.querySelector('[data-path="' + fragment.path.join('.') + '"]').innerText.substr(fragment.startOffset, fragment.endOffset) + ' ';
+      }
+    });
+    joinedText = joinedText.replace('\n', '').trim();
+
+    return (joinedText.length > 0)?joinedText:null;
+  }
+
   execute(params, context) {
+
+    params.quoteMessage = this.getSelectionText(params.selection) || params.quoteMessage;
+
     var state = this.getCommandState();
-    console.info('Added quote "' + params.quoteMessage + '" with author: "' + params.quoteAuthor + '" to document');
+
     if (state.disabled) {
       return;
     }
