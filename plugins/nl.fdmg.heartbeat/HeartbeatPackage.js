0ff1ea036277cba2cf7e1d73b46d4e80631c52e9
Willem Liu <willem.liu@fdmediagroep.nl>
2017-01-10 06:07:34 +0100

    WBWRI-830 Changed translation

--- plugins/nl.fdmg.heartbeat/HeartbeatPackage.js
+++ plugins/nl.fdmg.heartbeat/HeartbeatPackage.js
@@ -16,7 +16,7 @@ export default {
     config.addLabel('No heartbeat', { nl: 'Geen heartbeat' });
     config.addLabel('Article unlocked', { nl: 'Artikel vrij' });
     config.addLabel('Article is new.', { nl: 'Nieuw artikel.' });
-    config.addLabel('New article', { nl: 'Nieuw artikel.' });
+    config.addLabel('New article', { nl: 'Nieuw artikel' });
     config.addLabel('Heartbeat endpoint is unreachable. Article is or will become unlocked in less than 70 seconds.', {
       nl: 'Heartbeat endpoint is onbereikbaar. Artikel is of zal vrij komen in minder dan 70 seconden.'
     });
