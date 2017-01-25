8314cdbf18302c9f4c33dc0306c23385da19fc7d
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2017-01-25 13:58:11 +0100

    Changed preview to voorvertoning

--- plugins/nl.fdmg.preview/PreviewPackage.js
+++ plugins/nl.fdmg.preview/PreviewPackage.js
@@ -5,6 +5,9 @@ export default {
   id: 'nl.fdmg.preview',
   name: 'preview',
   configure: function (config) {
-    config.addComponentToSidebarWithTabId(this.id, 'main', PreviewComponent)
+    config.addComponentToSidebarWithTabId(this.id, 'main', PreviewComponent);
+    config.addLabel('Preview', {
+      'nl': 'Voorvertoning'
+    });
   }
 }
