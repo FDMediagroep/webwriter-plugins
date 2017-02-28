3d1a2d4468af262bbd367ad6788ec6ea52d02de7
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2017-02-28 17:27:41 +0100

    Added semicolons to fdpersonal plugin

--- plugins/nl.fdmg.fdpersonal/index.js
+++ plugins/nl.fdmg.fdpersonal/index.js
@@ -1,9 +1,9 @@
-import FDPersonalPackage from './FDPersonalPackage'
-import {registerPlugin} from 'writer'
+import FDPersonalPackage from './FDPersonalPackage';
+import {registerPlugin} from 'writer';
 
 export default () => {
   if (registerPlugin) {
-    registerPlugin(FDPersonalPackage)
+    registerPlugin(FDPersonalPackage);
   } else {
     console.error("Register method not yet available");
   }
